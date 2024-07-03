797fcf00d599e3549c570a7648043a691b0d6e71
Augusto Cs <augustocsuarez1985@gmail.com>
2024-07-03 20:58:31 +1000

    change url

--- src/store/slices/weather/thunks.js
+++ src/store/slices/weather/thunks.js
@@ -4,7 +4,7 @@ export const getWeatheryCity = (city) =>{
     return async (dispatch)=>{
         dispatch(startloading())
         try {
-            const response = await fetch(`http://localhost:3000/api/weather/${city}`)
+            const response = await fetch(`https://weather-api-ten-wine.vercel.app/api/weather/${city}`)
             const {main,weather,name,wind} = await response.json();
             console.log('response', main);
             dispatch(setWeatherData({main,weather,name,wind}))
